c18654b0fc02aa7b9ab145e5fd49f2d4fcc7d52f
Matt Buckley <matthew.buckley@wsp.com>
2023-12-06 15:25:35 +0000

    GTFS-rt database summary insert timings
    Extract database row insert times from AVL log file and plot.

--- src/scripts/gtfs_database_summary.py
+++ src/scripts/gtfs_database_summary.py
@@ -6,10 +6,14 @@
 
 ##### IMPORTS #####
 # Standard imports
+import dataclasses
+import datetime as dt
 import logging
 import pathlib
+import re
 import textwrap
-from typing import Optional
+import warnings
+from typing import Any, Optional
 
 # Third party imports
 import numpy as np
@@ -17,19 +21,41 @@ import pandas as pd
 import pydantic
 import sqlalchemy
 from caf import toolkit as ctk
+from matplotlib import dates as mdates
+from matplotlib import figure, patches
+from matplotlib import pyplot as plt
 from matplotlib.backends import backend_pdf
-from matplotlib import pyplot as plt, figure, patches
 from sqlalchemy import sql
 
 # Local imports
 from bodse.avl import database
 
 ##### CONSTANTS #####
-LOG = logging.getLogger(__name__)
-CONFIG_PATH = pathlib.Path("gtfs_database_summary.yml")
+
+_ROOT_NAME = "bodse"
+_SCRIPT_NAME = pathlib.Path(__file__).stem
+_TOOL_NAME = f"{_ROOT_NAME}.{_SCRIPT_NAME}"
+LOG = logging.getLogger(_TOOL_NAME)
+CONFIG_PATH = pathlib.Path(__file__).with_suffix(".yml")
 
 
 ##### CLASSES #####
+
+
+@dataclasses.dataclass
+class _InsertTimeColumns:
+    """Names of columns in the insert timings dataframe."""
+
+    ID = "id"
+    COUNT = "current_count"
+    INSERT_TIME = "insert_time"
+    COMPLETE_TIME = "complete_time"
+    TIME_TAKEN = "time_taken_seconds"
+    POSITIONS = "response_positions_count"
+    CUMM_POSITIONS = "cummulative_positions_count"
+    OUTLIER = "outlier_mask"
+
+
 class SummaryConfig(ctk.BaseConfig):
     """Parameters for running GTFS database summaries."""
 
@@ -281,14 +307,244 @@ def _compare_timestamps(
     LOG.info("Written: %s", out_path)
 
 
-def main():
-    parameters = SummaryConfig.load_yaml(CONFIG_PATH)
+def _extract_insert_timings(
+    log_file: pathlib.Path, output_folder: pathlib.Path, sd_filter: float = 2
+) -> tuple[pd.DataFrame, pathlib.Path]:
+    """Parse AVL log file to determine time to insert rows into database.
+
+    Parameters
+    ----------
+    log_file : pathlib.Path
+        Path to log file.
+    output_folder : pathlib.Path
+        Folder to save output spreadsheet to.
+    sd_filter : float, default 2.0
+        Number of standard deviations away from mean to
+        flag insert times as outliers.
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame (index is arbitary) containing columns defined
+        in `_InsertTimeColumns`.
+    pathlib.Path
+        Path to Excel file produced.
+    """
+    LOG.info('Extracting insert times from log file: "%s"', log_file)
+    log_line = re.compile(
+        r"^(?P<datetime>\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2})"  # date & time
+        r" \[(?P<module>[\s\w\.\d]+)\]"  # Python module
+        r" \[(?P<level>[\w\s]+)\]"  # Log level
+        r"(?P<message>.*)$",  # log message
+        re.I,
+    )
+    datetime_format = "%d-%m-%Y %H:%M:%S"
+
+    id_pattern = re.compile(r"^\s*Inserting feed into database with ID: (\d+).*$")
+    insert_pattern = re.compile(r"^\s*inserting (\d+) positions.*$", re.I)
+    complete_pattern = re.compile(r"^\s*(\d+) complete.*$", re.I)
+
+    data: list[dict] = []
+    row: dict[str, Any] = {}
+
+    with open(log_file, "rt", encoding="utf-8") as file:
+        for line in file:
+            line_match = log_line.match(line)
+            if line_match is None:
+                continue
+
+            if line_match.group("level").lower().strip() != "info":
+                continue
+
+            id_match = id_pattern.match(line_match.group("message"))
+            if id_match is not None:
+                row[_InsertTimeColumns.ID] = int(id_match.group(1))
+                continue
+
+            insert_match = insert_pattern.match(line_match.group("message"))
+            if insert_match is not None:
+                row[_InsertTimeColumns.INSERT_TIME] = dt.datetime.strptime(
+                    line_match.group("datetime"), datetime_format
+                )
+                row[_InsertTimeColumns.POSITIONS] = int(insert_match.group(1))
+                continue
+
+            complete_match = complete_pattern.match(line_match.group("message"))
+            if complete_match is not None:
+                row[_InsertTimeColumns.COUNT] = int(complete_match.group(1))
+                row[_InsertTimeColumns.COMPLETE_TIME] = dt.datetime.strptime(
+                    line_match.group("datetime"), datetime_format
+                )
+
+                try:
+                    row[_InsertTimeColumns.TIME_TAKEN] = (
+                        row[_InsertTimeColumns.COMPLETE_TIME]
+                        - row[_InsertTimeColumns.INSERT_TIME]
+                    ).total_seconds()
+                except KeyError as exc:
+                    warnings.warn(
+                        "insert time taken cannot be calculated because some values "
+                        f"are missing ({exc}), incomplete row will be included in data"
+                    )
+
+                data.append(row)
+                row = {}
+
+    if len(row) != 0:
+        warnings.warn(
+            "log data found without final complete line, "
+            "this incomplete row will be included in data"
+        )
+        data.append(row)
+        row = {}
+
+    dataset = pd.DataFrame(data)
+    dataset.loc[:, _InsertTimeColumns.CUMM_POSITIONS] = dataset[
+        _InsertTimeColumns.POSITIONS
+    ].cumsum()
+
+    LOG.info(
+        "Log file ranges from initial insert on %s to final complete on %s",
+        dataset[_InsertTimeColumns.INSERT_TIME].min(),
+        dataset[_InsertTimeColumns.COMPLETE_TIME].max(),
+    )
+
+    # Create column for outliers > X s.d.
+    mean_time = dataset[_InsertTimeColumns.TIME_TAKEN].mean()
+    sd_time = dataset[_InsertTimeColumns.TIME_TAKEN].std()
+    dataset.loc[:, _InsertTimeColumns.OUTLIER] = (
+        dataset[_InsertTimeColumns.TIME_TAKEN] < (mean_time - sd_filter * sd_time)
+    ) | (dataset[_InsertTimeColumns.TIME_TAKEN] > (mean_time + sd_filter * sd_time))
+    LOG.info(
+        "Flagged %s outliers which are more than %.1f "
+        "standard deviations (%.1f) away from the mean (%.1f)",
+        dataset[_InsertTimeColumns.OUTLIER].sum(),
+        sd_filter,
+        sd_filter * sd_time,
+        mean_time,
+    )
+
+    time_summary = pd.DataFrame(
+        {
+            "time_taken_seconds": dataset[_InsertTimeColumns.TIME_TAKEN].describe(),
+            "time_taken_without_outliers": dataset.loc[
+                ~dataset[_InsertTimeColumns.OUTLIER], _InsertTimeColumns.TIME_TAKEN
+            ].describe(),
+        }
+    )
+
+    out_file = output_folder / "database_insert_timings.xlsx"
+    # pylint: disable=abstract-class-instantiated
+    with pd.ExcelWriter(out_file, engine="openpyxl") as excel:
+        dataset.to_excel(excel, sheet_name="DB Insert Times", index=False)
+        time_summary.to_excel(excel, sheet_name="Time Summary")
+    LOG.info("Written: %s", out_file)
+
+    return dataset, out_file
+
+
+def _rolling_average(array: np.ndarray, n: int):
+    return np.convolve(array, np.ones(n), "valid") / n
+
+
+def _insert_timings_figure(
+    x: np.ndarray,
+    y: np.ndarray,
+    mean_period: int,
+    xlabel: str,
+    ylabel: str,
+    title: str,
+    xdata_date: bool = False,
+) -> figure.Figure:
+    """Create scatter plot with rolling mean."""
+    ax: plt.Axes
+    fig, ax = plt.subplots(figsize=(10, 8), layout="tight")
+
+    ax.scatter(x, y, c="C0", s=3)
+
+    # period should be odd so the offset calculation is correct
+    if mean_period % 2 == 0:
+        raise ValueError(f"mean_period should be an odd number, not {mean_period}")
+
+    offset = mean_period // 2
+    ax.plot(
+        x[offset:-offset],
+        _rolling_average(y, mean_period),
+        label=f"Rolling Mean ({mean_period})",
+        c="C1",
+    )
+
+    if xdata_date:
+        locator = mdates.AutoDateLocator(minticks=3, maxticks=7)
+        formatter = mdates.ConciseDateFormatter(locator)
+        ax.xaxis.set_major_locator(locator)
+        ax.xaxis.set_major_formatter(formatter)
+
+    ax.legend()
+    ax.set_ylim(0)
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
+    ax.set_title(title)
+
+    return fig
+
+
+def _plot_insert_timings(timings: pd.DataFrame, output_file: pathlib.Path) -> None:
+    """Create scatterplot, with rolling mean, of database insert times.
+
+    Parameters
+    ----------
+    timings : pd.DataFrame
+        DataFrame (index is arbitary) containing columns defined
+        in `_InsertTimeColumns`.
+    output_file : pathlib.Path
+        PDF file to save plot to.
+
+    See Also
+    --------
+    _extract_insert_timings
+    """
+    LOG.info("Plotting insert timings")
+    timings = timings.loc[~timings[_InsertTimeColumns.OUTLIER]]
+
+    period = 21
+    ylabel = "Insert Time Taken (seconds)"
+
+    plot_parameters = (
+        ("Response ID", _InsertTimeColumns.ID, False),
+        ("Insert Time", _InsertTimeColumns.INSERT_TIME, True),
+        ("No. Rows in Database", _InsertTimeColumns.CUMM_POSITIONS, False),
+    )
+
+    with backend_pdf.PdfPages(output_file.with_suffix(".pdf")) as pdf:
+        for label, column, date_fmt in plot_parameters:
+            fig = _insert_timings_figure(
+                timings[column].values,
+                timings[_InsertTimeColumns.TIME_TAKEN].values,
+                period,
+                label,
+                ylabel,
+                "AVL Database Row Insert Times Over Time,\n"
+                "with Increasing Total Rows in the Database",
+                xdata_date=date_fmt,
+            )
+            pdf.savefig(fig)
+            plt.close(fig)
+
+    LOG.info("Written: %s", output_file)
+
+
+def main(parameters: SummaryConfig, output_folder: pathlib.Path):
     db_path = parameters.avl_output_folder / "gtfs-rt.sqlite"
+    log_file = parameters.avl_output_folder / "AVL_downloader.log"
 
     # Setup matplotlib style parameters
     plt.style.use("bmh")
     plt.rcParams["font.family"] = "monospace"
     plt.rcParams["patch.facecolor"] = "white"
+
+    insert_timings, excel_file = _extract_insert_timings(log_file, output_folder)
+    _plot_insert_timings(insert_timings, excel_file.with_suffix(".pdf"))
 
     gtfs_db = database.GTFSRTDatabase(db_path)
 
@@ -304,10 +560,11 @@ def main():
         _compare_timestamps(
             conn,
             *parameters.metadata_ids,
-            parameters.avl_output_folder / "GTFS-rt_timestamp_distribution.png",
+            output_folder / "GTFS-rt_timestamp_distribution.png",
         )
 
-    out_file = parameters.avl_output_folder / "GTFS-rt_summary.xlsx"
+    out_file = output_folder / "GTFS-rt_summary.xlsx"
+    # pylint: disable=abstract-class-instantiated
     with pd.ExcelWriter(out_file, engine="openpyxl") as excel:
         for name, data in summaries.items():
             data.columns = data.columns.str.title()
@@ -315,5 +572,20 @@ def main():
     LOG.info("Written: %s", out_file)
 
 
+def _run() -> None:
+    """Load config, setup logger and run `main`."""
+    parameters = SummaryConfig.load_yaml(CONFIG_PATH)
+    output_folder = parameters.avl_output_folder / _SCRIPT_NAME
+    output_folder.mkdir(exist_ok=True)
+
+    tool_details = ctk.ToolDetails(_TOOL_NAME, "0.0.0")
+    with ctk.LogHelper(
+        _ROOT_NAME,
+        tool_details=tool_details,
+        log_file=output_folder / (CONFIG_PATH.stem + ".log"),
+    ):
+        main(parameters, output_folder)
+
+
 if __name__ == "__main__":
-    main()
+    _run()
